455e3bd34ed70eaff28b0734ba3689a268e7e027
Jagdeep-17 <11827jagdeepsingh@gmail.com>
2026-08-08 13:55:28 +0530

    feat: refactor Home and HomeHeading components; update layout for improved structure and styling

--- src/components/Home/HomeHeading.jsx
+++ src/components/Home/HomeHeading.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const HomeHeading = () => {
   return (
-    <div className='h-full flex justify-center items-center flex-col gap-12'>
-  <h3 className='font-[Caligraphy] text-7xl font-bold  tracking-wide mb-5' >Jagdeep Singh</h3>
-  <p className='font-NotoSans uppercase text-xl text-stone-900 tracking-widest'>I learn, build, and never settle.</p>
+    <div className="min-h-screen flex justify-center items-center flex-col gap-12">
+      <h3 className="font-[Caligraphy] text-7xl font-bold tracking-wide mb-5">
+        Jagdeep Singh
+      </h3>
 
-</div>
-  )
-}
+      <p className="font-NotoSans uppercase text-xl text-stone-900 tracking-widest">
+        I learn, build, and never settle.
+      </p>
+    </div>
+  );
+};
 
-export default HomeHeading
+export default HomeHeading;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import HomeHeading from '../components/Home/HomeHeading'
-import Collage_Layer from '../components/Home/Collage_Layer'
+import React from "react";
+import HomeHeading from "../components/Home/HomeHeading";
+import Collage_Layer from "../components/Home/Collage_Layer";
 
 const Home = () => {
   return (
-    <div className='h-screen w-full relative overflow-y-visible mb-24'>
-<HomeHeading/>
-<Collage_Layer/>
-</div>
-  )
-}
+    <section className="relative min-h-screen w-full overflow-visible">
+      {/* Hero text */}
+      <div className="relative z-20 min-h-screen">
+        <HomeHeading />
+      </div>
 
-export default Home
+      {/* 3D + collage layer */}
+      <Collage_Layer />
+    </section>
+  );
+};
+
+export default Home;
